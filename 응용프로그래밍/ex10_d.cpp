b39f31590e8f86152b6a4dd6b60488f17b876537
93Gunit <lodr5016@gmail.com>
2016-09-23 12:07:57 +0900

    응프 4번쨰 과제 미완성
    Signed-off-by: 93Gunit <lodr5016@gmail.com>

---
+++ 응용프로그래밍/ex10_d.cpp
@@ -0,0 +1,31 @@
+/// for while 
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	printf(" 출력할 행의 개수(0~26 범위의 정수)를 입력하세요.  ");
+	scanf(" %d", &n);
+
+	int i,j,k;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = n - 1; j > 0; j--)
+		{
+			printf(" ");
+		}
+		j = 0;
+		for (k = 1; k <=i; k++)
+		{
+			printf("%c", 123 - k);
+			
+		}
+		k = 0;
+		printf("\n");
+	}
+
+	return 0;
+}
